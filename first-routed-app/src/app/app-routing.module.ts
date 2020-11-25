f99dc72919d1a6f8d6e6f2aba3d16592ba762b78
mattiatosoni <tosoni.mattia@iisgalvanimi.edu.it>
2020-11-25 12:02:08 +0000

    01.1 metà

--- first-routed-app/src/app/app-routing.module.ts
+++ first-routed-app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = []; //Qui scriveremo le route
+const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'search', component: SearchComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
